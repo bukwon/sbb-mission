0ea895478f5468472f957aaa9da9b7dfe40a95d5
bukwon <dnu05043@naver.com>
2023-11-27 14:36:15 +0900

    3-2

--- src/test/java/com/sbbproject/sbbproject/SbbProjectApplicationTests.java
+++ src/test/java/com/sbbproject/sbbproject/SbbProjectApplicationTests.java
@@ -1,21 +1,9 @@
 package com.sbbproject.sbbproject;
 
-import com.sbbproject.sbbproject.answer.Answer;
-import com.sbbproject.sbbproject.answer.AnswerService;
-import com.sbbproject.sbbproject.question.Question;
-import com.sbbproject.sbbproject.question.QuestionRepository;
 import com.sbbproject.sbbproject.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -27,7 +15,7 @@ class SbbApplicationTests {
     void testJpa() {
         for (int i = 1; i <= 300; i++) {
             String subject = String.format("테스트 데이터입니다:[%03d]", i);
-            String content = "내용 무";
+            String content = "내용무";
             this.questionService.create(subject, content);
         }
     }
