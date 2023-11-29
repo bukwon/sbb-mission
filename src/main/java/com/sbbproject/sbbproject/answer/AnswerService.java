be48da1aec0e2a3097737226155c6993912cdbce
bukwon <dnu05043@naver.com>
2023-11-29 11:35:04 +0900

    3-10

--- src/main/java/com/sbbproject/sbbproject/answer/AnswerService.java
+++ src/main/java/com/sbbproject/sbbproject/answer/AnswerService.java
@@ -1,11 +1,13 @@
 package com.sbbproject.sbbproject.answer;
 
+import com.sbbproject.sbbproject.DataNotFoundException;
 import com.sbbproject.sbbproject.question.Question;
 import com.sbbproject.sbbproject.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -20,5 +22,24 @@ public class AnswerService {
         answer.setAuthor(author);
         this.answerRepository.save(answer);
         return answer;
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
